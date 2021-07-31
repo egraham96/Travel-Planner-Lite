0730591c114a43b4e6664dd4c28e0fdb3075e1ec
skchung93 <skchung93@gmail.comgit config --global user.email>
2021-07-31 12:49:48 -0700

    committing javascript that we worked on today. Update that allows us to pull data for restaurants, attractions, and hotels. We can add images, but right now we are getting a 429 (Too Many Requests) error. Need to figure out a delay function

--- script2.js
+++ script2.js
@@ -2,6 +2,11 @@ var openWeatherAPIKey = '96097dc8f5f71e97eb432223ff032ed9';
 var currentWeatherConditions = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var citySearch = document.querySelector('.search-input');
 var searchButton = document.querySelector('.submit-button');
+
+var hotelSection = document.querySelector('#hotel');
+var restaurantSection = document.querySelector('#rest');
+var attractionSection = document.querySelector('#sites');
+
 var latitude ='';
 var longitude ='';
 
@@ -17,7 +22,7 @@ function userInput(){
     })
 }    
 
-//function to grab data from oneWeather API
+//function to grab data from oneWeather API and then push the data into the hotel, restaurant and attractions functions.
 function currentCityWeather(){
     city = citySearch.value;
     var currentWeatherURL = currentWeatherConditions + city + '&appid=' + openWeatherAPIKey + '&units=imperial';
@@ -29,47 +34,181 @@ function currentCityWeather(){
     
             var latLonWeatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly,alerts&units=imperial&appid=' + openWeatherAPIKey;
             console.log(data);
-            travelAdvisor(latitude, longitude);
 
+            //calling the Hotel, attractions, and restaurant functions to get list of respective category for city we are searching for.
+            hotelAdvisor(latitude, longitude);
+            attractionsAdvisor(latitude, longitude);
+            restaurantsAdvisor(latitude, longitude);
         })
 }        
 
-//function for hotel travel advisor API (NO DATA REALLY)
-function travelAdvisor(latitude, longitude){
+//function for list of hotels in CITY
+function hotelAdvisor(latitude, longitude){
 
-    fetch("https://priceline-com.p.rapidapi.com/hotels/city/nearby/" + latitude + "/" + longitude + "?radius=100", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
-            "x-rapidapi-host": "priceline-com.p.rapidapi.com"
-        }
-    })
-    .then(response => response.json())
-    .then((data) => {
-        console.log(data);
-})  
-    .catch(err => {
-        console.error(err);
-    });
-}
-
-//function for hotel TRY 2
-function travelAdvisor2(latitude, longitude){
-    fetch("https://travel-advisor.p.rapidapi.com/hotels/list-by-latlng?latitude=" + latitude + "&longitude=" + longitude +"&lang=en_US&hotel_class=1%2C2%2C3&limit=30&adults=1&amenities=beach%2Cbar_lounge%2Cairport_transportation&rooms=1&child_rm_ages=7%2C10&currency=USD&zff=4%2C6&subcategory=hotel%2Cbb%2Cspecialty&nights=2", {
+    fetch("https://travel-advisor.p.rapidapi.com/hotels/list-by-latlng?latitude=" + latitude + "&longitude=" + longitude + "&lang=en_US&hotel_class=3&limit=10&adults=2&amenities=bar_lounge&rooms=1&currency=USD&subcategory=hotel%2Cbb&nights=5", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
 		"x-rapidapi-host": "travel-advisor.p.rapidapi.com"
 	}
-})
-.then(response => response.json())
-.then((data) => {
+    })
+    .then(response => response.json())
+    .then((data) => {
         console.log(data);
-})
-.catch(err => {
-	console.error(err);
-});
+        var hotelLocationID = [];
+        for (i=0; i< data.data.length; i++){
+            var hotelID = data.data[i].location_id;
+            hotelLocationID.push(hotelID);
+        }    
+        console.log(hotelLocationID);
+
+        //For Loop to Call images and then add them onto the html.
+        for(j=0; j<5; j++){
+            fetch("https://travel-advisor.p.rapidapi.com/photos/list?location_id=" + hotelLocationID[j] + "&currency=USD&limit=2&lang=en_US", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
+                "x-rapidapi-host": "travel-advisor.p.rapidapi.com"
+            }
+            })
+            .then(response => response.json())
+            .then((data) => {
+                    console.log(data);
+                    var imageTags = document.createElement('img');
+                    var locationImg = data.data[0].images.small.url;
+                    imageTags.setAttribute('src', locationImg);
+                    hotelSection.append(imageTags);
+                    
+            })
+            .catch(err => {
+            console.error(err);
+            });
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
 }
+
+//function for list of attractions in CITY
+function attractionsAdvisor(latitude, longitude){
+    fetch("https://travel-advisor.p.rapidapi.com/attractions/list-by-latlng?longitude=" + longitude + " &latitude=" + latitude + " &lunit=mi&currency=USD&limit=10&lang=en_US", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
+		"x-rapidapi-host": "travel-advisor.p.rapidapi.com"
+	}
+    })
+    .then(response => response.json())
+    .then((data) => {
+            console.log(data);
+            var attractionLocationID = [];
+            
+            for (i=0; i< data.data.length; i++){
+                var attractionID = data.data[i].location_id;
+                attractionLocationID.push(attractionID);
+            } 
+            console.log(attractionLocationID);
+            //For Loop to Call images and then add them onto the html.
+            for(j=0; j<5; j++){
+                fetch("https://travel-advisor.p.rapidapi.com/photos/list?location_id=" + attractionLocationID[j] + "&currency=USD&limit=2&lang=en_US", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
+                    "x-rapidapi-host": "travel-advisor.p.rapidapi.com"
+                }
+                })
+                .then(response => response.json())
+                .then((data) => {
+                        console.log(data);
+                        var imageTags = document.createElement('img');
+                        var locationImg = data.data[0].images.small.url;
+                        imageTags.setAttribute('src', locationImg);
+                        attractionSection.append(imageTags);
+                        
+                })
+                .catch(err => {
+                console.error(err);
+                });
+            }
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+//function for list of restaurants in CITY
+function restaurantsAdvisor(latitude, longitude){
+    fetch("https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude=" + latitude + "&longitude=" + longitude + "&limit=10&currency=USD&distance=2&open_now=false&lunit=km&lang=en_US&min_rating=4", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
+		"x-rapidapi-host": "travel-advisor.p.rapidapi.com"
+	}
+    })
+    .then(response => response.json())
+    .then((data) => {
+            console.log(data);
+            var restaurantLocationID = [];
+            for (i=0; i< data.data.length; i++){
+                var restaurantID = data.data[i].location_id;
+                restaurantLocationID.push(restaurantID);
+            }
+            console.log(restaurantLocationID);
+            //For Loop to Call images and then add them onto the html.
+            for(j=0; j<5; j++){
+                fetch("https://travel-advisor.p.rapidapi.com/photos/list?location_id=" + restaurantLocationID[j] + "&currency=USD&limit=2&lang=en_US", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
+                    "x-rapidapi-host": "travel-advisor.p.rapidapi.com"
+                }
+                })
+                .then(response => response.json())
+                .then((data) => {
+                        console.log(data);
+                        var imageTags = document.createElement('img');
+                        var locationImg = data.data[0].images.small.url;
+                        imageTags.setAttribute('src', locationImg);
+                        restaurantSection.append(imageTags);
+                        
+                })
+                .catch(err => {
+                console.error(err);
+                });
+            }
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}    
+
+//function for photos of hotel/restaurant/attractions
+// function photosAdvisor(array){
+//     for(j=0; j<5; j++){
+//         fetch("https://travel-advisor.p.rapidapi.com/photos/list?location_id=" + array[j] + "&currency=USD&limit=2&lang=en_US", {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-key": "e1c4800408msh394f782ebf22b7bp16879cjsn040eb2b0eac5",
+//             "x-rapidapi-host": "travel-advisor.p.rapidapi.com"
+//         }
+//         })
+//         .then(response => response.json())
+//         .then((data) => {
+//                 console.log(data);
+//                 var imageTags = document.createElement('img');
+//                 var locationImg = data.data[0].images.small.url;
+//                 imageTags.setAttribute('src', locationImg);
+//                 hotelSection.append(imageTags);
+                
+//         })
+//         .catch(err => {
+//         console.error(err);
+//         });
+//     }
+// }
+
 
 
 
